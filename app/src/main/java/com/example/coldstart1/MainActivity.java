57b929bff37300dd73c0390130f44997fe86d353
Ilya111-lab <67420211+Ilya111-lab@users.noreply.github.com>
2022-09-02 22:59:47 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/coldstart1/MainActivity.java
+++ app/src/main/java/com/example/coldstart1/MainActivity.java
@@ -45,28 +45,7 @@ public class MainActivity extends AppCompatActivity {
         nMyApplication = (MyApplication) getApplication();
       nMyApplication.onActivityCreated(this, savedInstanceState);
         Log.v(LOG_TAG, "Number of entry:" + nMyApplication.i);
-   //     getApplication().registerActivityLifecycleCallbacks(this);
-      //  registerActivityLifecycleCallbacks(new MyActivityLifecycleCallbacks());
-         /*   ActivityCompat.requestPermissions( this,
-                    new String[]{
-                            Manifest.permission.READ_EXTERNAL_STORAGE,
-                            Manifest.permission.MANAGE_EXTERNAL_STORAGE
-                    }, 1
-            );
-            if (Environment.isExternalStorageManager()){
-// If you don't have access, launch a new activity to show the user the system's dialog
-// to allow access to the external storage
-            }else{
-                Intent intent = new Intent();
-                intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-                Uri uri = Uri.fromParts("package", this.getPackageName(), null);
-                intent.setData(uri);
-                startActivity(intent);
-            }*/
-            int[] price1 = {0};
-      //  int[] price2 = decryptData(price1,50,1,8);
-     //   System.out.println(Arrays.toString(price2));
-        if(nMyApplication.i==3)    Toast.makeText(this, "Started!", Toast.LENGTH_LONG).show();
+          if(nMyApplication.i==3)    Toast.makeText(this, "Started!", Toast.LENGTH_LONG).show();
 
     }
 
@@ -96,35 +75,4 @@ public class MainActivity extends AppCompatActivity {
             Log.e("","onActivityStopped:" + activity.getLocalClassName());
         }
     }
-  /*  public @Nullable
-    int[] decryptData(@NonNull int[] price,
-                      @IntRange(from = 1) int discount,
-                      @IntRange(from = 0) int offset,
-                      @IntRange(from = 1) int readLength) {int flagNegative=0;
-        if((offset+readLength<=price.length)&&(discount<=99)&&(discount>=1)&&(offset>=0)&&(readLength>=1))
-        {int resultPrice[] = new int[readLength];
-                    for (int i = 0; i < readLength; i++) {
-                resultPrice[i]=price[offset+i]*discount/100;
-                if(resultPrice[i]<0) flagNegative++;
-            } String intArrayString = Arrays.toString(resultPrice);
-            System.out.println(intArrayString);
-        if (flagNegative==0)   return resultPrice;
-                else System.out.println("Prices<0");return null;}
-        else if((offset+readLength>=price.length)&&(offset<price.length))
-        {int resultPrice[] = new int[price.length-offset];
-            for (int i = 0; i < price.length-offset; i++) {
-                resultPrice[i]=price[offset+i]*discount/100;
-                if(resultPrice[i]<0) flagNegative++;
-        }String intArrayString = Arrays.toString(resultPrice);
-            System.out.println(intArrayString);
-            if (flagNegative==0)   return resultPrice;
-            else System.out.println("Prices<0");return null;}
-        else{System.out.println("Received argument is not appropriate");
-            return null;}
-    }*/
-
-
-
-
- //
-/*  */
+ 
